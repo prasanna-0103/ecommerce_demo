2eb9e60c07ac9ac795d788c8e7100edcf055c145
prasanna-0103 <71935496+prasanna-0103@users.noreply.github.com>
2023-03-02 15:48:24 +0530

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Online Store</title>
+    <meta charset="utf-8" />
+    <meta name="viewpoint" content="width=device-width,initial-scal=1.0">
+    <meta http-equip="X-UA-compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/style1.css" media="screen" type="text/css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script type="text/javascript" src="jquery.store.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+    <section id="nav-bar">
+        <nav class="navbar navbar-expand-lg navbar-light">
+
+            
+            <div class="collapse navbar-collapse" id="navbarNav">
+
+                <ul class="navbar-nav ml-auto">
+
+                    <li class="nav-item">
+                        <a style="color:black;" class="nav-link" href="login1.php"  >Login</a>
+                    </li>
+                    
+
+
+                    <li class="nav-item">
+                        <a style="color:black;" class="nav-link" href="#about">About Webroot</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a style="color:black;" class="nav-link" href="#services">Plans</a>
+                    </li>
+
+                  
+                    <p>
+                         
+                    </p>
+
+
+                </ul>
+            </div>
+        </nav>
+
+
+
+        <!------About Section------->
+        <section id="about">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2>Welcome to Webroot </h2>
+                        <div class="about-content">
+                            Our antivirus software is
+                            constantly protecting your devices.
+                            With lightning-fast scans, Webroot will keep you secure and won't slow you down.
+                            Webroot* is ranked #1 in overall performance.
+
+                        </div>
+
+                        <div>
+                            * Webroot SecureAnywhere® AntiVirus
+
+                        </div>
+
+
+                        <!-- <button type="button" class="btn btn-primary">Read More>>
+			
+			</button> -->
+
+                    </div>
+                    <img src="images/webroot1.png">
+                </div>
+
+
+
+            </div>
+
+
+            <!------Images Section------->
+            <section class="new-arrivals">
+                <div id="site">
+                    <div class="container">
+                       
+                        <div class="row">
+                            <div class="col-md-3">
+                                <div class="product-top">
+                                    <img src="images/ab.png">
+
+                                </div>
+
+
+                                <div class="product-bottom text-center">
+
+                                    <h3>Consistently outperforms
+                                        all major competitors</h3>
+
+                                </div>
+                            </div>
+
+
+                            <div class="col-md-3">
+                                <div class="product-top">
+                                    <img src="images/bc.png">
+
+                                </div>
+
+
+                                <div class="product-bottom text-center">
+
+                                    <h3>Quick to install
+                                        35x faster</h3>
+
+                                </div>
+                            </div>
+
+
+
+                            <div class="col-md-3">
+                                <div class="product-top">
+                                    <img src="images/df.png">
+
+                                </div>
+
+
+                                <div class="product-bottom text-center">
+
+                                    <h3>Scans in a flash
+                                        6.8x faster</h3>
+
+                                </div>
+                            </div>
+
+
+
+                            <div class="col-md-3">
+                                <div class="product-top">
+                                    <img src="images/ab.png">
+
+                                </div>
+
+
+                                <div class="product-bottom text-center">
+
+                                    <h3>Consistent protection</h3>
+
+                                </div>
+                            </div>
+
+                        </div>
+            </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <!------About Section------->
+            <section id="about">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h2>About Us</h2>
+                            <div class="about-content">
+                                Webroot, an OpenText company, was the first to harness the cloud and artificial intelligence to stop zero-day threats in real time. Webroot secures businesses and individuals worldwide with threat intelligence and protection for endpoints and networks.
+
+                                In 2019, Webroot and its parent company Carbonite were acquired by OpenText, a global leader in Enterprise Information Management. Together, we are a market leader in cyber resilience, offering total endpoint protection and disaster recovery for any sized business.
+
+
+                            </div>
+                            <!-- <button type="button" class="btn btn-primary">Read More>>
+			
+			</button> -->
+
+                        </div>
+                        <div class="col-md-6 skills-bar">
+                            <p>Delivery Rate</p>
+                            <div class="progress">
+                                <div class="progress-bar" style="width:85%;">85%</div>
+                            </div>
+
+                            <p>Customer Growth</p>
+                            <div class="progress">
+                                <div class="progress-bar" style="width:75%;">75%</div>
+                            </div>
+
+                            <p>Happy Employee</p>
+                            <div class="progress">
+                                <div class="progress-bar" style="width:90%;">90%</div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+<!------Services Section------->		
+<section id="services">
+	
+    <div class="container">
+        <h1>Our Plans</h1>
+        <div class="row services" >
+            <div class= "col-md-4 text-center">
+                <div class="icon">
+                   
+                <i class="fa fa-laptop"></i>
+                </div>
+                <h3> AntiVirus </h3>
+                <h3>  </h3>
+                <p>Protect 1 device for 1 year</p>
+            </div>
+        
+            <div class="col-md-4 text-center">
+                <div class="icon">
+                <i class="fa fa-laptop"></i>
+                </div>
+                <h3> Internet Security Plus with AntiVirus</h3>
+                <p>Protect 3 devices for 1 year</p>
+            </div>
+        
+            <div class="col-md-4 text-center">
+                <div class="icon">
+                <i class="fa fa-laptop"></i>
+                </div>
+                <h3>Internet Security Complete with AntiVirus</h3>
+                <p>Protect 5 devices for 1 year</p>
+            </div>
+        </div>
+    </div>
+
+    
+
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+               
+
+               
+                <!-- Quantity: <input type="number" value="1" name="quantity" style="width: 20%"/><br>
+    	<input type="hidden" value="'.$row['id'].'" name="ac"/> -->
+
+
+</body>
+
+</html>
